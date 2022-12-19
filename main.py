426394cacaaba3425987760d1990b7734dfcfadf
AlexanderF048 <alexandr.frankevich@gmail.com>
2022-12-19 22:45:39 +0100

    Changed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import urllib.parse
 import socket
 from threading import Thread
 from datetime import datetime
+import json
 
 
 class HttpHandler(BaseHTTPRequestHandler):
@@ -24,7 +25,7 @@ class HttpHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         data = self.rfile.read(int(self.headers['Content-Length']))
-        form_client_run(data, '127.0.0.1', 5000)
+        form_client_run(data, 'localhost', 5000)
         self.send_response(302)
         self.send_header('Location', '/')
         self.end_headers()
@@ -46,7 +47,6 @@ class HttpHandler(BaseHTTPRequestHandler):
                 self.send_static()
             else:
                 self.send_html_file('error.html', 404)
-
         self.end_headers()
         with open(f'.{self.path}', 'rb') as file:
             self.wfile.write(file.read())
@@ -63,24 +63,47 @@ def run(server_class=HTTPServer, handler_class=HttpHandler):
 
 def form_socket_run():
     serv_socket_form = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    serv_socket_form.bind(('127.0.0.1', 5000))
+    serv_socket_form.bind(('localhost', 5000))
+
     try:
         while True:
-            data, address = serv_socket_form.recvfrom(1024)
-            print(f'Received data: {data.decode()} from: {address}')
+            data, address_port = serv_socket_form.recvfrom(1024)
+            print(data, address_port)
+            print(f'Received data: {data.decode()} from: {address_port}')
             data_parse = urllib.parse.unquote_plus(data.decode())
             # print(data_parse)
-            data_dict = {
-                datetime.datetime.now(): {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}}
+            time_mark = datetime.utcnow()
+            print(time_mark)
+            data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
             print(data_dict)
-            with open('test.txt', 'wb') as txt_test:
-                txt_test.write(data)
+            print(data_dict)
+            with open('test.txt', 'w') as txt_test:
+                txt_test.write(str(data_dict))
 
+            json_dict_pattern = {str(datetime.utcnow()): data_dict}
+
+            try:
+
+                with open('storage/wishes.json', 'r') as j_file:
+                    work_file = json.load(j_file)
+                    w = dict(work_file)
+
+                w[str(datetime.utcnow())] = data_dict
+
+                with open('storage/wishes.json', 'w') as j_file:
+                    json.dump(w, j_file)
+                    print('NO exeption JSON')
+
+            except:
+                print('exeption JSON')
+                with open('storage/wishes.json', 'w') as j_file:
+                    json.dump(json_dict_pattern, j_file)
 
     except KeyboardInterrupt:
         print(f'Destroy server')
     finally:
         serv_socket_form.close()
+        print("Socket port 5000 finally closed!!!")
 
 
 def form_client_run(data, ip, port):
@@ -88,14 +111,14 @@ def form_client_run(data, ip, port):
     server = ip, port
     sock.sendto(data, server)
     print(f'Send data: {data.decode()} to server: {server}')
-    response, address = sock.recvfrom(1024)
-    print(f'Response data: {response.decode()} from address: {address}')
+    # response, address = sock.recvfrom(1024) # Если включить этот код то без ответа клиент не пойдет дальше.
+    # print(f'Response data: {response.decode()} from address: {address}')
     sock.close()
+    print("client socket CLOSED")
 
 
 if __name__ == '__main__':
     http_serv_thread = Thread(target=run, args=())
     socket_serv_thread = Thread(target=form_socket_run, args=())
-
-    http_serv_thread.start()
     socket_serv_thread.start()
+    http_serv_thread.start()
